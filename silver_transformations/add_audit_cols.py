f88993fdb731392d5b7923eab637c8012d61ead9
MayursGit <MayursGit>
2025-10-02 08:58:32 +0000

    update in add_audit_cols functions

--- silver_transformations/add_audit_cols.py
+++ silver_transformations/add_audit_cols.py
@@ -4,6 +4,6 @@ import datetime
 
 def add_audit_cols(df,filename):
     def _(df):
-        df = df.withColumn("batch_id",lit(filename)).withColumn("create_date",lit(current_date())).withColumn("create_user",lit('cpprod')).withColumn("update_date",lit(current_date())).withColumn("update_user",lit(None))
+        df = df.withColumn("batch_id",lit(filename)).withColumn("create_date",lit(current_date())).withColumn("create_user",lit('cpprod')).withColumn("update_date",lit(current_date())).withColumn("update_user",lit(None).cast('string'))
         return df
     return _
