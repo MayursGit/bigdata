0313015adb24ebb2db3d5b07760ece0fd0a53903
Mayur Sonawane <mayur10594@gmail.com>
2025-10-02 15:12:01 +0530

    adding a test commit from VS code

--- silver_transformations/add_audit_cols.py
+++ silver_transformations/add_audit_cols.py
@@ -2,6 +2,8 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 import datetime 
 
+#this is for testing commit in git
+
 def add_audit_cols(df,filename):
     def _(df):
         df = df.withColumn("batch_id",lit(filename)).withColumn("create_date",lit(current_date())).withColumn("create_user",lit('cpprod')).withColumn("update_date",lit(current_date())).withColumn("update_user",lit(None).cast('string'))
